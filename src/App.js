41d723cb148e4e0732de9101f073b6e7afe12d51
Abelard Bautista <abelardbautista@gmail.com>
2020-08-18 23:44:39 -0700

    Changed name of Upload to Receipt. Added border for better visualization.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import Upload from "./Components/Upload";
+import Receipt from "./Components/Receipt";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 // import logo from "./logo.svg";
 // import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <Upload />
-    </div>
+    <Container>
+      <Receipt />
+    </Container>
   );
 }
 
---
+++ src/Components/Receipt.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Receipt = () => {
+  return (
+    <Row>
+      <Col style={{ borderStyle: "solid" }}>Receipt Goes Here</Col>
+    </Row>
+  );
+};
+
+export default Receipt;
