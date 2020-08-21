e995b9a5f78cda81948d107997363345784a6efc
Abelard Bautista <abelardbautista@gmail.com>
2020-08-21 02:48:59 -0700

    Added position for future 'total' component.

--- src/Components/Receipt.js
+++ src/Components/Receipt.js
@@ -3,7 +3,29 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Receipt = () => {
-  return <Col>Receipt Component Here</Col>;
+  return (
+    <Col>
+      Receipt Component Here
+      <Row>
+        <Col
+          style={{ borderStyle: "solid" }}
+          xs={8}
+          sm={8}
+          md={8}
+          lg={8}
+          xl={8}
+        >
+          Line Item
+        </Col>
+        <Col style={{ borderStyle: "solid" }}>Price</Col>
+      </Row>
+      <Row>
+        <Col style={{ borderStyle: "solid" }} lg={{ span: 4, offset: 8 }}>
+          Test
+        </Col>
+      </Row>
+    </Col>
+  );
 };
 
 export default Receipt;
